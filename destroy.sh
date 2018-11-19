d234155bda43e1f4320a6b91d7cd494d845bd1d7
Chris Smith <grizzthedj@gmail.com>
2018-11-19 10:10:10 -0500

    Adding prometheus grafana yamls

--- destroy.sh
+++ destroy.sh
@@ -2,4 +2,8 @@
 kubectl delete -f halyard/halyard.yml 
 kubectl delete -f halyard/serviceaccount.yml
 kubectl delete -f minio/standalone.yml
-kubectl delete namespace spinnaker
+
+#kubectl delete namespace spinnaker
+
+kubectl delete -f https://raw.githubusercontent.com/giantswarm/kubernetes-prometheus/master/manifests-all.yaml
+
